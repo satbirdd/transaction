f3e143bd6185321b022ab8865508199d3e57ca11
liulei <liulei@liulei-NF520-A2G.(none)>
2013-04-18 00:09:13 +0800

    change

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,11 +4,11 @@ class Order < ActiveRecord::Base
   has_many :order_items
   accepts_nested_attributes_for :order_items
 
-  after_update :update_to_valid, if: :been_confirmed
+  after_update :update_items, if: :been_confirmed
 
   protected
 
-  def update_to_valid
+  def update_items
   	order_items.each do |item|
   		item.update_attributes(status: 1)
   	end
